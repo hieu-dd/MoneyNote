1a6d6cf2891a72fa423f2e73d56329167ba99a2c
14H <doduchieu.kstn@gmail.com>
2022-05-09 09:59:16 +0700

    Update search categories

--- lib/providers/category/categories.dart
+++ lib/providers/category/categories.dart
@@ -5,10 +5,14 @@ import 'package:money_note/providers/category/const.dart' as Const;
 class CategoriesProvider with ChangeNotifier {
   List<Category> _categories = [];
 
-  Future<List<Category>> getCategories() async {
+  Future<List<Category>> getCategories(String text) async {
     if (_categories.isEmpty) {
       _categories = Const.categories;
     }
-    return _categories;
+    return _categories
+        .where(
+          (element) => element.name.toLowerCase().contains(text.toLowerCase()),
+        )
+        .toList();
   }
 }
--- lib/ui/category/categories_screen.dart
+++ lib/ui/category/categories_screen.dart
@@ -13,12 +13,20 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
-  var _searchText = "";
+  var _displayCategories = [];
   final _searchController = TextEditingController();
 
-  void _searchCategory(String text) {
+  @override
+  void initState() {
+    _searchCategories('');
+    super.initState();
+  }
+
+  void _searchCategories(String text) async {
+    final categoriesProvider = context.read<CategoriesProvider>();
+    final displayCategories = await categoriesProvider.getCategories(text);
     setState(() {
-      _searchText = text;
+      _displayCategories = displayCategories;
     });
   }
 
@@ -33,69 +41,56 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final categoriesProvider = context.watch<CategoriesProvider>();
     final theme = Theme.of(context);
-    return FutureBuilder(
-        future: categoriesProvider.getCategories(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            final categories = snapshot.data as List<Category>;
-            final displayCategories = categories
-                .where((element) => element.name.contains(_searchText))
-                .toList();
-            return Scaffold(
-              appBar: AppBar(
-                leading: IconButton(
-                  icon: Icon(
-                    Icons.arrow_back,
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: theme.primaryColor,
+          ),
+          onPressed: () {
+            _onBack(context);
+          },
+        ),
+        backgroundColor: Colors.white,
+        title: TextField(
+          controller: _searchController,
+          onChanged: (text){
+            _searchCategories(text);
+          },
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Type a category\'s name',
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                final category = _displayCategories[index];
+                return ListTile(
+                  onTap: () {
+                    _onSelect(context, category);
+                  },
+                  leading: Icon(
+                    category.icon,
                     color: theme.primaryColor,
                   ),
-                  onPressed: () {
-                    _onBack(context);
-                  },
-                ),
-                backgroundColor: Colors.white,
-                title: TextField(
-                  controller: _searchController,
-                  onChanged: _searchCategory,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Type a category\'s name',
+                  title: Text(category.name),
+                  shape: const Border(
+                    bottom: BorderSide(width: 0.1),
+                    top: BorderSide.none,
                   ),
-                ),
-              ),
-              body: Column(
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                      itemBuilder: (context, index) {
-                        final category = displayCategories[index];
-                        return ListTile(
-                          onTap: () {
-                            _onSelect(context, category);
-                          },
-                          leading: Icon(
-                            category.icon,
-                            color: theme.primaryColor,
-                          ),
-                          title: Text(category.name),
-                          shape: const Border(
-                            bottom: BorderSide(width: 0.1),
-                            top: BorderSide.none,
-                          ),
-                        );
-                      },
-                      itemCount: displayCategories.length,
-                    ),
-                  )
-                ],
-              ),
-            );
-          }
-        });
+                );
+              },
+              itemCount: _displayCategories.length,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
