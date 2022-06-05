2039e6ffbfa2062ba8c2d28db90bbb20454ec77f
14H <doduchieu.kstn@gmail.com>
2022-06-05 15:44:14 +0700

    Add first unittest

---
+++ test/utils/ext/string_ext_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:money_note/utils/ext/string_ext.dart';
+
+void main() {
+  test('Test capitalize', () {
+    const testString = "abc";
+    final expectString = testString.capitalize();
+    expect(expectString, "Abc");
+  });
+}
